3421cece255ce22de52956db68a713df5bdbc802
Richy McGregor <Deepfriedice@gmail.com>
2021-02-23 08:24:53 +1000

    replaced operations with emit

--- compile.lua
+++ compile.lua
@@ -1,5 +1,5 @@
 require "ksum"
-require "operations"
+require "emit"
 
 BLOCK_LEN = 10
 
@@ -41,22 +41,16 @@ end
 function start_block (prog, cstate)
 
 	if cstate.root == 0 then
-		table.insert(prog, ops.noop)
-		print(#prog, "noop")
+		emit.noop(prog)
 
 	elseif not cstate.cond then
-		table.insert(prog, ops.seek_n(2))
-		print(#prog, "seek", 2)
-		table.insert(prog, ops.seek_n(BLOCK_LEN))
-		print(#prog, "seek", BLOCK_LEN)
+		emit.seek(prog, 2)
+		emit.seek(prog, BLOCK_LEN)
 
 	else
-		table.insert(prog, ops.seek_n(3))
-		print(#prog, "seek", 3)
-		table.insert(prog, ops.seek_n(BLOCK_LEN))
-		print(#prog, "seek", BLOCK_LEN)
-		table.insert(prog, ops.seek_n(BLOCK_LEN))
-		print(#prog, "seek", BLOCK_LEN)
+		emit.seek(prog, 3)
+		emit.seek(prog, BLOCK_LEN)
+		emit.seek(prog, BLOCK_LEN)
 
 	end
 end
@@ -69,8 +63,7 @@ function compile_body (prog, cstate, c)
 	elseif c == '{' then
 		cstate.buffer = 0
 		cstate.mode = "state"
-		table.insert(prog, ops.noop)  -- ensure enter will be aligned
-		print(#prog, "noop")
+		emit.noop(prog)  -- ensure enter will be aligned
 
 	else
 		error("Invalid body command: " .. c)
@@ -104,13 +97,11 @@ function compile_instr (prog, cstate, c)
 
 	elseif c == '}' then
 		if cstate.cond then error("unfinished condition") end
-		table.insert(prog, ops.jump_n(cstate.root))
-		print(#prog, "jump", cstate.root)
+		emit.jump(prog, cstate.root)
 		cstate.root = 0
 		cstate.mode = "body"
 		while #prog % BLOCK_LEN ~= 0 do
-			table.insert(prog, ops.noop)
-			print(#prog, "noop")
+			emit.noop(prog)
 		end
 
 	elseif c == '?' then
@@ -118,68 +109,54 @@ function compile_instr (prog, cstate, c)
 		cstate.cond = true
 		local block_start = #prog - (#prog % BLOCK_LEN) + 1
 		local skip_dest = block_start + BLOCK_LEN + 2
-		table.insert(prog, ops.cond(skip_dest))
-		print(#prog, "cond", skip_dest)
+		emit.cond(prog, skip_dest)
 
 	elseif c == ';' then
 		if not cstate.cond then error("free condition end") end
 		cstate.cond = false
 		while #prog % BLOCK_LEN ~= 0 do
-			table.insert(prog, ops.noop)
-			print(#prog, "noop")
+			emit.noop(prog)
 		end
 
 	elseif c == '+' then
-		table.insert(prog, ops.add)
-		print(#prog, "+")
+		emit.add(prog)
 
 	elseif c == '=' then
-		table.insert(prog, ops.eq)
-		print(#prog, "=")
+		emit.eq(prog)
 
 	elseif c == '>' then
-		table.insert(prog, ops.gt)
-		print(#prog, ">")
+		emit.gt(prog)
 
 	elseif c == '<' then
-		table.insert(prog, ops.lt)
-		print(#prog, "<")
+		emit.lt(prog)
 
 	elseif c == '!' then
-		table.insert(prog, ops.lnot)
-		print(#prog, "not")
+		emit.lnot(prog)
 
 	elseif c == '.' then
-		table.insert(prog, ops.write)
-		print(#prog, "write")
+		emit.write(prog)
 
 	elseif c == 'c' then
-		table.insert(prog, ops.copy)
-		print(#prog, "copy")
+		emit.copy(prog)
 
 	elseif c == 's' then
-		table.insert(prog, ops.swap)
-		print(#prog, "swap")
+		emit.swap(prog)
 
 	elseif c == 'r' then
-		table.insert(prog, ops.jump_n(cstate.root))
-		print(#prog, "jump", cstate.root)
+		emit.jump(prog, cstate.root)
 
 	elseif c == '$' then
 		cstate.root = #prog + 1
 
 	elseif 'W' <= c and c <= 'Z' then
 		local r = string.lower(c)
-		table.insert(prog, ops.set_r(r))
-		print(#prog, "set " .. r)
+		emit.set_reg(prog, r)
 
 	elseif 'w' <= c and c <= 'x' then
-		table.insert(prog, ops.get_r(c))
-		print(#prog, "get " .. c)
+		emit.get_reg(prog, c)
 
 	elseif c == '@' then
-		table.insert(prog, ops.switch)
-		print(#prog, "switch")
+		emit.switch(prog)
 
 	else
 		error("Invalid instruction: " .. c)
@@ -189,8 +166,7 @@ end
 
 function compile_char (prog, cstate, c)
 	local n = string.byte(c)
-	table.insert(prog, ops.push_n(n))
-	print(#prog, "push", n)
+	emit.push(prog, n)
 	cstate.mode = 'instr'
 end
 
@@ -201,8 +177,7 @@ function compile_deci (prog, cstate, c)
 	elseif '0' <= c and c <= '9' then
 		cstate.buffer = 10 * cstate.buffer + c:byte() - 48
 	elseif  c == 'D' then
-		table.insert(prog, ops.push_n(cstate.buffer))
-		print(#prog, "push", cstate.buffer)
+		emit.push(prog, cstate.buffer)
 		cstate.mode = 'instr'
 	else
 		error("Invalid decimal: " .. c)
@@ -220,8 +195,7 @@ function compile_hexa (prog, cstate, c)
 	elseif 'a' <= c and c <= 'f' then
 		cstate.buffer = 16 * cstate.buffer + c:byte() - string.byte('a') + 10
 	elseif  c == 'H' then
-		table.insert(prog, ops.push_n(cstate.buffer))
-		print(#prog, "push", cstate.buffer)
+		emit.push(prog, cstate.buffer)
 		cstate.mode = 'instr'
 	else
 		error("Invalid hexadecimal: " .. c)
@@ -238,8 +212,7 @@ function compile_label (prog, cstate, c)
 			'a' <= c and c <= 'z' then
 		cstate.buffer = ksum(cstate.buffer, c:byte())
 	elseif c == ')' then
-		table.insert(prog, ops.push_n(cstate.buffer))
-		print(#prog, "push", cstate.buffer)
+		emit.push(prog, cstate.buffer)
 		cstate.mode = 'instr'
 	else
 		error("Invalid label: " .. c)
@@ -258,8 +231,7 @@ function compile_state (prog, cstate, c)
 	elseif c == ':' then
 		local block_start = #prog - (#prog % BLOCK_LEN) + 1
 		local skip_dest = block_start + BLOCK_LEN + 1
-		table.insert(prog, ops.enter(cstate.buffer, skip_dest))
-		print(#prog, "enter", cstate.buffer, skip_dest)
+		emit.enter(prog, cstate.buffer, skip_dest)
 		cstate.root = #prog
 		cstate.mode = 'instr'
 	else
@@ -270,9 +242,7 @@ end
 
 function compile_stop (prog, cstate)
 	while #prog % BLOCK_LEN ~= 1 do
-		table.insert(prog, ops.noop)
-		print(#prog, "noop")
+		emit.noop(prog)
 	end
-	table.insert(prog, ops.exit)
-	print(#prog, "exit")
+	emit.exit(prog)
 end
---
+++ emit.lua
@@ -0,0 +1,217 @@
+DEBUG_EMIT_PRINT = true
+
+
+function debug_print(...)
+	if DEBUG_EMIT_PRINT then
+		print(...)
+	end
+end
+
+
+emit = {}
+
+
+function emit.noop (prog)
+	debug_print(#prog + 1, "noop")
+	table.insert(prog, function (state)
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.push (prog, n)
+	debug_print(#prog + 1, "push " .. n)
+	table.insert(prog, function (state)
+		table.insert(state.dstack, n)
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.add (prog)
+	debug_print(#prog + 1, "add")
+	table.insert(prog, function (state)
+		state.ip = state.ip + 1
+		local i = table.remove(state.dstack)
+		local j = table.remove(state.dstack)
+		table.insert(state.dstack, i + j)
+	end)
+end
+
+
+function emit.eq (prog)
+	debug_print(#prog + 1, "eq")
+	table.insert(prog, function (state)
+		local i = table.remove(state.dstack)
+		local j = table.remove(state.dstack)
+		if j == i then
+			table.insert(state.dstack, -1)
+		else
+			table.insert(state.dstack, 0)
+		end
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.gt (prog)
+	debug_print(#prog + 1, "gt")
+	table.insert(prog, function (state)
+		local i = table.remove(state.dstack)
+		local j = table.remove(state.dstack)
+		if j > i then
+			table.insert(state.dstack, -1)
+		else
+			table.insert(state.dstack, 0)
+		end
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.lt (prog)
+	debug_print(#prog + 1, "lt")
+	table.insert(prog, function (state)
+		local i = table.remove(state.dstack)
+		local j = table.remove(state.dstack)
+		if j < i then
+			table.insert(state.dstack, -1)
+		else
+			table.insert(state.dstack, 0)
+		end
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.lnot (prog)
+	debug_print(#prog + 1, "lnot")
+	table.insert(prog, function (state)
+		local i = table.remove(state.dstack)
+		if i ~= 0 then
+			table.insert(state.dstack, -1)
+		else
+			table.insert(state.dstack, 0)
+		end
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.swap (prog)
+	debug_print(#prog + 1, "swap")
+	table.insert(prog, function (state)
+		local len = #state.dstack
+		state.dstack[len - 1], state.dstack[len] = state.dstack[len], state.dstack[len - 1]
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.copy (prog)
+	debug_print(#prog + 1, "copy")
+	table.insert(prog, function (state)
+		local tos = state.dstack[#state.dstack]
+		table.insert(state.dstack, tos)
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.write (prog)
+	debug_print(#prog + 1, "write")
+	table.insert(prog, function (state)
+		local n = table.remove(state.dstack)
+		local c = string.char(n)
+		io.write(c)
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.read (prog)
+	debug_print(#prog + 1, "read")
+	table.insert(prog, function (state)
+		local c = io.read(1)
+		local n = string.byte(c)
+		table.insert(state.dstack, n)
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.exit (prog)
+	debug_print(#prog + 1, "exit")
+	table.insert(prog, function (state)
+		state.running = false
+	end)
+end
+
+
+function emit.jump (prog, dest)
+	debug_print(#prog + 1, "jump" .. dest)
+	table.insert(prog, function (state)
+		state.ip = dest
+	end)
+end
+
+
+function emit.seek (prog, n)
+	local dest = #prog + n + 1
+	emit.jump(prog, dest)
+end
+
+
+function emit.enter (prog, label, dest)
+	debug_print(#prog + 1, "enter " .. label .. " " .. dest)
+	table.insert(prog, function (state)
+		if state.label == label then
+			state.ip = state.ip + 1
+		else
+			state.ip = dest
+		end
+	end)
+end
+
+
+function emit.switch (prog)
+	debug_print(#prog + 1, "switch")
+	table.insert(prog, function (state)
+		state.label = table.remove(state.dstack)
+		state.ip = 1
+	end)
+end
+
+
+function emit.cond (prog, skip_dest)
+	debug_print(#prog + 1, "cond " .. skip_dest)
+	table.insert(prog, function (state)
+		local n = table.remove(state.dstack)
+		if n ~= 0 then
+			state.cond = true
+			state.ip = state.ip + 1
+		else
+			state.ip = skip_dest
+		end
+	end)
+end
+
+
+function emit.set_reg (prog, reg)
+	debug_print(#prog + 1, "set " .. reg)
+	table.insert(prog, function (state)
+		local n = table.remove(state.dstack)
+		state.reg[reg] = n
+		state.ip = state.ip + 1
+	end)
+end
+
+
+function emit.get_reg (prog, reg)
+	debug_print(#prog + 1, "get " .. reg)
+	table.insert(prog, function (state)
+		local n = state.reg[reg]
+		table.insert(state.dstack, n)
+		state.ip = state.ip + 1
+	end)
+end
