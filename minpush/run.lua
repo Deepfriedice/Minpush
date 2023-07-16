0fc4d9dd08af6388ab2f213cb9e277b0da73932c
Richy McGregor <Deepfriedice@gmail.com>
2023-07-16 14:03:01 +1000

    split off new_state in run

--- minpush/run.lua
+++ minpush/run.lua
@@ -1,6 +1,26 @@
 require "ksum"
 
 
+function new_state (input, output)
+	if input == nil then
+		input = io.input()
+	end
+	if output == nil then
+		output = io.output()
+	end
+	return {
+		input = input,
+		output = output,
+		ip = 1,
+		running = true,
+		stack = {},
+		array = {},
+		label = ksum_text("start"),
+		reg = { w = 0, x = 0, y = 0, z = 0 },
+	}
+end
+
+
 function step(prog, state)
 	local ip = state.ip
 	assert(ip <= #prog, "out of bounds: " .. ip)
@@ -10,25 +30,7 @@ end
 
 
 function run(prog, input, output)
-	if input == nil then
-		input = io.input()
-	end
-	if output == nil then
-		output = io.output()
-	end
-
-	local state = {
-		input = input,
-		output = output,
-		ip = 1,
-		running = true,
-		dstack = {},
-		astack = {},
-		alstack = {},
-		label = ksum_text("start"),
-		reg = { w = 0, x = 0, y = 0, z = 0 },
-	}
-
+	local state = new_state(input, output)
 	while state.running do
 		step(prog, state)
 	end
